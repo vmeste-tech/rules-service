bec42281eb6b6b4e6bd3e3eb45fbe94d743511f4
kolpakovee <eekolpakov@edu.hse.ru>
2025-03-13 18:34:52 +0300

    init spring project

---
+++ src/test/java/ru/kolpakovee/rules_service/RulesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kolpakovee.rules_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
