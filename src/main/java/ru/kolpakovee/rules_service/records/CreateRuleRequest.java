3db6ed62194063760b5a103c666f8ea2ccd53f8b
kolpakovee <eekolpakov@edu.hse.ru>
2025-03-20 12:24:47 +0300

    Update request: add constraints

--- src/main/java/ru/kolpakovee/rules_service/records/CreateRuleRequest.java
+++ src/main/java/ru/kolpakovee/rules_service/records/CreateRuleRequest.java
@@ -1,10 +1,23 @@
 package ru.kolpakovee.rules_service.records;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record CreateRuleRequest(
+        @NotBlank(message = "Name must not be blank")
         String name,
+
+        @NotBlank(message = "Description must not be blank")
         String description,
+
+        @PositiveOrZero(message = "Penalty amount must be zero or positive")
         double penaltyAmount,
+
+        @NotBlank(message = "Cron expression must not be blank")
         String cronExpression,
+
+        @NotBlank(message = "Time zone must not be blank")
         String timeZone
 ) {
 }
+
